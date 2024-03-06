7f9ce97d3bd43421bcfdc0c07f5cb44ad8843b1c
강지원 <kangji149@naver.com>
2024-03-06 23:33:08 +0900

    feat : add request remittance controller

---
+++ remittance-service/src/main/java/com/example/remittance/adapter/in/RequestRemittanceController.java
@@ -0,0 +1,10 @@
+package com.example.remittance.adapter.in;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RequestRemittanceController {
+
+}
